acca6e823a739ecd9796493f040f1125dea234f4
Erik Osterman <e@osterman.com>
2018-06-07 23:01:32 -0700

    Initial implementation

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_domain_name" {
+  value = "${module.s3_bucket.bucket_domain_name}"
+}
+
+output "bucket_id" {
+  value = "${module.s3_bucket.bucket_id}"
+}
+
+output "bucket_arn" {
+  value = "${module.s3_bucket.bucket_arn}"
+}
+
+output "bucket_prefix" {
+  value = "${module.s3_bucket.prefix}"
+}
