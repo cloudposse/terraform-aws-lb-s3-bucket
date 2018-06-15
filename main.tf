9d80b2d29599f876b80e3bd5e7a3c9a70717556b
Erik Osterman <erik@cloudposse.com>
2018-06-15 12:49:51 -0700

    Initial implementation (#1)
    * Initial implementation
    
    * Drop prefix
    
    * restore prefix but default to empty string
    
    * add explicit types to vars and fix up docs
    
    * update to slack

---
+++ main.tf
@@ -0,0 +1,49 @@
+data "aws_elb_service_account" "default" {}
+
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = "${var.attributes}"
+  tags       = "${var.tags}"
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid = ""
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_elb_service_account.default.arn}"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${module.label.id}/*",
+    ]
+  }
+}
+
+module "s3_bucket" {
+  source                 = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.2.0"
+  namespace              = "${var.namespace}"
+  stage                  = "${var.stage}"
+  name                   = "${var.name}"
+  region                 = "${var.region}"
+  acl                    = "${var.acl}"
+  policy                 = "${data.aws_iam_policy_document.default.json}"
+  force_destroy          = "${var.force_destroy}"
+  versioning_enabled     = "true"
+  lifecycle_rule_enabled = "false"
+  delimiter              = "${var.delimiter}"
+  attributes             = "${var.attributes}"
+  tags                   = "${var.tags}"
+  prefix                 = "${var.prefix}"
+}
