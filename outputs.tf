91b5e016386b74ced62cdbc5e7ef95b84f0df17a
Sarkis Varozian <svarozian@gmail.com>
2018-06-21 08:01:41 -0700

    add description for outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "bucket_domain_name" {
-  value = "${module.s3_bucket.bucket_domain_name}"
+  description = "S3 bucket domain name"
+  value       = "${module.s3_bucket.bucket_domain_name}"
 }
 
 output "bucket_id" {
-  value = "${module.s3_bucket.bucket_id}"
+  description = "S3 bucket ID"
+  value       = "${module.s3_bucket.bucket_id}"
 }
 
 output "bucket_arn" {
-  value = "${module.s3_bucket.bucket_arn}"
+  description = "S3 bucket ARN"
+  value       = "${module.s3_bucket.bucket_arn}"
 }
 
 output "bucket_prefix" {
-  value = "${module.s3_bucket.prefix}"
+  description = "S3 bucket prefix"
+  value       = "${module.s3_bucket.prefix}"
 }
