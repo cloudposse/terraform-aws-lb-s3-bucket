0aebea294f84a22892f30676253f3fd942be7a39
Mike Hoskins <unixdoes@gmail.com>
2020-06-30 14:51:17 -0400

    expose s3 bucket lifecycle_rule_enabled (#11)
    * expose s3 bucket lifecycle_rule_enabled
    
    * expose retention related vars
    
    * document new inputs
    
    * Updated README.md
    
    * expose retention related vars
    
    * bump upstream module version
    
    * Updated README.md
    
    * Update main.tf
    
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>
    Co-authored-by: Erik Osterman <erik@cloudposse.com>
    Co-authored-by: PePe Amengual <jose.amengual@gmail.com>

--- docs/terraform.md
+++ docs/terraform.md
@@ -20,14 +20,21 @@
 | acl | Canned ACL to apply to the S3 bucket | `string` | `"log-delivery-write"` | no |
 | attributes | Additional attributes (e.g. `logs`) | `list(string)` | `[]` | no |
 | delimiter | Delimiter to be used between `namespace`, `stage`, `name` and `attributes` | `string` | `"-"` | no |
+| enable\_glacier\_transition | Enables the transition of lb logs to AWS Glacier | `bool` | `true` | no |
 | enabled | Set to `false` to prevent the module from creating any resources | `bool` | `true` | no |
 | environment | Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT' | `string` | `""` | no |
+| expiration\_days | Number of days after which to expunge s3 logs | `number` | `90` | no |
 | force\_destroy | A boolean that indicates the bucket can be destroyed even if it contains objects. These objects are not recoverable | `bool` | `false` | no |
+| glacier\_transition\_days | Number of days after which to move s3 logs to the glacier storage tier | `number` | `60` | no |
 | lifecycle\_prefix | Prefix filter. Used to manage object lifecycle events | `string` | `""` | no |
+| lifecycle\_rule\_enabled | A boolean that indicates whether the s3 log bucket lifecycle rule should be enabled. | `bool` | `false` | no |
 | name | Name  (e.g. `app` or `cluster`) | `string` | n/a | yes |
 | namespace | Namespace (e.g. `cp` or `cloudposse`) | `string` | `""` | no |
+| noncurrent\_version\_expiration\_days | Specifies when noncurrent s3 log versions expire | `number` | `90` | no |
+| noncurrent\_version\_transition\_days | Specifies when noncurrent s3 log versions transition | `number` | `30` | no |
 | region | AWS Region for S3 bucket | `string` | n/a | yes |
 | stage | Stage (e.g. `prod`, `dev`, `staging`) | `string` | `""` | no |
+| standard\_transition\_days | Number of days to persist logs in standard storage tier before moving to the infrequent access tier | `number` | `30` | no |
 | tags | Additional tags (e.g. map(`BusinessUnit`,`XYZ`) | `map(string)` | `{}` | no |
 
 ## Outputs
--- main.tf
+++ main.tf
@@ -38,20 +38,26 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_bucket" {
-  source                 = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.7.0"
-  enabled                = var.enabled
-  namespace              = var.namespace
-  stage                  = var.stage
-  environment            = var.environment
-  name                   = var.name
-  region                 = var.region
-  acl                    = var.acl
-  policy                 = join("", data.aws_iam_policy_document.default.*.json)
-  force_destroy          = var.force_destroy
-  versioning_enabled     = true
-  lifecycle_rule_enabled = false
-  delimiter              = var.delimiter
-  attributes             = var.attributes
-  tags                   = var.tags
-  lifecycle_prefix       = var.lifecycle_prefix
+  source                             = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.11.0"
+  enabled                            = var.enabled
+  namespace                          = var.namespace
+  stage                              = var.stage
+  environment                        = var.environment
+  name                               = var.name
+  region                             = var.region
+  acl                                = var.acl
+  policy                             = join("", data.aws_iam_policy_document.default.*.json)
+  force_destroy                      = var.force_destroy
+  versioning_enabled                 = true
+  lifecycle_rule_enabled             = var.lifecycle_rule_enabled
+  enable_glacier_transition          = var.enable_glacier_transition
+  expiration_days                    = var.expiration_days
+  glacier_transition_days            = var.glacier_transition_days
+  noncurrent_version_expiration_days = var.noncurrent_version_expiration_days
+  noncurrent_version_transition_days = var.noncurrent_version_transition_days
+  standard_transition_days           = var.standard_transition_days
+  delimiter                          = var.delimiter
+  attributes                         = var.attributes
+  tags                               = var.tags
+  lifecycle_prefix                   = var.lifecycle_prefix
 }
