1ac6b8e6f698660b074246ca41df8a1560139ec7
Julien Parent-Trudeau <julien.parent.trudeau@gmail.com>
2021-01-09 12:58:11 -0500

    Bump s3-log-storage to 0.15.1

--- main.tf
+++ main.tf
@@ -26,7 +26,8 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_bucket" {
-  source                             = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.14.0"
+  source                             = "cloudposse/s3-log-storage/aws"
+  version                            = "0.15.1"
   context                            = module.this.context
   acl                                = var.acl
   policy                             = join("", data.aws_iam_policy_document.default.*.json)
