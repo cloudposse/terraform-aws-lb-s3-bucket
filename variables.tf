57e14dca9b4886751caa7bd0b4e6047249e28757
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-27 18:48:09 -0400

    Deprecate inputs, add force_destroy_enabled

--- variables.tf
+++ variables.tf
@@ -6,56 +6,26 @@ variable "acl" {
 
 variable "force_destroy" {
   type        = bool
-  description = "A boolean that indicates the bucket can be destroyed even if it contains objects. These objects are not recoverable"
   default     = false
+  description = <<-EOT
+    When `true`, permits a non-empty S3 bucket to be deleted by first deleting all objects in the bucket.
+    THESE OBJECTS ARE NOT RECOVERABLE even if they were versioned and stored in Glacier.
+    Must be set `false` unless `force_destroy_enabled` is also `true`.
+    EOT
 }
 
-variable "lifecycle_prefix" {
-  type        = string
-  description = "Prefix filter. Used to manage object lifecycle events"
-  default     = ""
-}
-
-variable "lifecycle_rule_enabled" {
+variable "force_destroy_enabled" {
   type        = bool
-  description = "A boolean that indicates whether the s3 log bucket lifecycle rule should be enabled."
   default     = false
-}
-
-variable "enable_glacier_transition" {
-  type        = bool
-  description = "Enables the transition of lb logs to AWS Glacier"
-  default     = true
-}
-
-variable "glacier_transition_days" {
-  type        = number
-  description = "Number of days after which to move s3 logs to the glacier storage tier"
-  default     = 60
-}
-
-variable "expiration_days" {
-  type        = number
-  description = "Number of days after which to expunge s3 logs"
-  default     = 90
-}
-
-variable "noncurrent_version_expiration_days" {
-  type        = number
-  description = "Specifies when noncurrent s3 log versions expire"
-  default     = 90
-}
-
-variable "noncurrent_version_transition_days" {
-  type        = number
-  description = "Specifies when noncurrent s3 log versions transition"
-  default     = 30
-}
-
-variable "standard_transition_days" {
-  type        = number
-  description = "Number of days to persist logs in standard storage tier before moving to the infrequent access tier"
-  default     = 30
+  description = <<-EOT
+    When `true`, permits `force_destroy` to be set to `true`.
+    This is an extra safety precaution to reduce the chance that Terraform will destroy and recreate
+    your S3 bucket, causing COMPLETE LOSS OF ALL DATA even if it was stored in Glacier.
+    WARNING: Upgrading this module from a version prior to 0.27.0 to this version
+      will cause Terraform to delete your existing S3 bucket CAUSING COMPLETE DATA LOSS
+      unless you follow the upgrade instructions on the Wiki [here](https://github.com/cloudposse/terraform-aws-s3-log-storage/wiki/Upgrading-to-v0.27.0-(POTENTIAL-DATA-LOSS)).
+      See additional instructions for upgrading from v0.27.0 to v0.28.0 [here](https://github.com/cloudposse/terraform-aws-s3-log-storage/wiki/Upgrading-to-v0.28.0-and-AWS-provider-v4-(POTENTIAL-DATA-LOSS)).
+    EOT
 }
 
 variable "access_log_bucket_name" {
@@ -68,4 +38,40 @@ variable "access_log_bucket_prefix" {
   type        = string
   description = "Prefix to prepend to the current S3 bucket name, where S3 access logs will be sent to"
   default     = null
+}
+
+variable "allow_ssl_requests_only" {
+  type        = bool
+  description = "Require requests to use Secure Socket Layer (HTTPS/SSL)."
+  default     = true
+}
+
+variable "versioning_enabled" {
+  type        = bool
+  description = "Enable object versioning, keeping multiple variants of an object in the same bucket"
+  default     = true
+}
+
+variable "lifecycle_configuration_rules" {
+  type = list(object({
+    enabled = bool
+    id      = string
+
+    abort_incomplete_multipart_upload_days = number
+
+    # `filter_and` is the `and` configuration block inside the `filter` configuration.
+    # This is the only place you should specify a prefix.
+    filter_and = any
+    expiration = any
+    transition = list(any)
+
+    noncurrent_version_expiration = any
+    noncurrent_version_transition = list(any)
+  }))
+  default     = []
+  description = <<-EOT
+    A list of S3 bucket v2 lifecycle rules, as specified in [terraform-aws-s3-bucket](https://github.com/cloudposse/terraform-aws-s3-bucket)"
+    These rules are not affected by the deprecated `lifecycle_rule_enabled` flag.
+    **NOTE:** Unless you also set `lifecycle_rule_enabled = false` you will also get the default deprecated rules set on your bucket.
+    EOT
 }
